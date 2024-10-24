193701373fe030235c51446b956de5f4d8d68e80
Sheep-heee <sheephi0609@gmail.com>
2024-10-24 23:48:41 +0900

    241024 setRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import ProjectDetail from './pages/ProjectDetail'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>,
+      },
+      {
+        path: "project/:id",
+        element: <ProjectDetail/>
+      }
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div className="bg-bgGrey">App---</div>
+    <div className="bg-bgGrey">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
